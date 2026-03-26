dcf6992959b14df93f4bbe9e89b0c7a7549b3e0f
barbox11 <123384500+barbox11@users.noreply.github.com>
2026-03-26 11:09:06 -0500

    se creo la conexion a base de datos con prisma, se agrego un endpoint de prueba para verificar la conexion a la base de datos y se agrego el archivo de configuracion de prisma.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { prisma } from "./config/prisma";
 
 const app = express();
 
@@ -13,6 +15,15 @@ app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
 res.send("API funcionando 🚀");
+});
+
+app.get("/test-db", async (req: Request, res: Response) => {
+    try {
+    const users = await prisma.user.findMany();
+    res.json({ message: "Conexión a BD OK ✅", users });
+    } catch (error) {
+    res.status(500).json({ error: "Error de conexión a BD ❌" });
+    }
 });
 
 app.listen(3000, () => {
